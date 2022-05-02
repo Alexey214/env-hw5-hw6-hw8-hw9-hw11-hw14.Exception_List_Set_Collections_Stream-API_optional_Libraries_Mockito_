fd1ad2421350bd18ff526ed152e765660228b5e6
brain <golenev214@gmail.com>
2022-05-02 15:35:30 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/envhw5/EnvHw5Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.envhw5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvHw5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvHw5Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course2/envhw5/EnvHw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.envhw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvHw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
