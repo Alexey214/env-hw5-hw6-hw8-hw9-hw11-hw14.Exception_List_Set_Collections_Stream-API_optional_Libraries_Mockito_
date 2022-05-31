1f0739680188a51c28dcb8783d1b713ced7b2678
brain <golenev214@gmail.com>
2022-05-31 16:46:51 +0300

    HW10 - result

--- src/main/java/pro/sky/java/course2/envhw5/EnvHw5Application.java
+++ src/main/java/pro/sky/java/course2/envhw5/EnvHw5Application.java
@@ -1,10 +1,8 @@
 package pro.sky.java.course2.envhw5;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Locale;
 
 @SpringBootApplication
 public class EnvHw5Application {
